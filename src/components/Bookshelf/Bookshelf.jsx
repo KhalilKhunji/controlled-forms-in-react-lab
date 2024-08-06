d91bdb083beb3a83c6e8dbe14bfcb15e5968afc7
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-06 16:50:21 +0300

    completed handleInputChange

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,11 +1,31 @@
 import {useState} from 'react';
 
 const Bookshelf = () => {
+    const [books, setBooks] = useState([{}]);
+    const [newBook, setNewBook] = useState({title: '', author: ''});
+    
+    const handleInputChange = (event) => {
+        let updatedBook = {...newBook};
+        if(event.target.name === 'title') {
+            updatedBook = {...newBook, title: event.target.value};
+        };
+        if(event.target.name === 'author') {
+            updatedBook = {...newBook, author: event.target.value};
+        };
+        setNewBook(updatedBook);
+    };
+
     return(
         <div className="bookshelfDiv">
             <div className="formDiv">
                 <h3>Add a Book</h3>
-                {/* Form will go here */}
+                <form>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" name="title" id="title" onChange={handleInputChange} />
+                    <label htmlFor="author">Author:</label>
+                    <input type="text" name="author" id="author" onChange={handleInputChange} />
+                    <button type="submit"></button>
+                </form>
             </div>
             <div className="bookCardsDiv">{/* Book cards will display here */}</div>
         </div>
